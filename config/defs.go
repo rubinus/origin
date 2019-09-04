7a6b360b86ce63125ae8b49ee19029bb9041e607
朱大仙儿 <zhuhonglei@zhuge.com>
2019-09-04 19:26:04 +0800

    add kafka

--- config/defs.go
+++ config/defs.go
@@ -1,14 +1,11 @@
 package config
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"git.zhugefang.com/gocore/zgo"
 	"io/ioutil"
-	"os"
 	"path/filepath"
-	"regexp"
 	"runtime"
 	"strconv"
 )
@@ -51,8 +48,6 @@ func initConfig(e, project, etcdHosts, port, rpcPort string) {
 		panic(err)
 	}
 
-	//Conf = LoadConfig(cf)
-
 	//使用zgo.Utils中的反序列化
 	err = zgo.Utils.Unmarshal(bf, &Conf)
 	if err != nil {
@@ -79,66 +74,4 @@ func initConfig(e, project, etcdHosts, port, rpcPort string) {
 	}
 
 	fmt.Printf("origin %s is started on the ... %s\n", Conf.Version, Conf.Env)
-}
-
-// LoadConfigByFile暂时不用
-func LoadConfigByFile(path string) *allConfig {
-	var config allConfig
-	configFile, err := os.Open(path)
-	if err != nil {
-		emit("Failed to open config file '%s': %s\n", path, err)
-		return &config
-	}
-
-	fi, _ := configFile.Stat()
-	if size := fi.Size(); size > (10 << 20) {
-		emit("config file (%q) size exceeds reasonable limit (%d) - aborting", path, size)
-		return &config // REVU: shouldn't this return an error, then?
-	}
-
-	if fi.Size() == 0 {
-		emit("config file (%q) is empty, skipping", path)
-		return &config
-	}
-
-	buffer := make([]byte, fi.Size())
-	_, err = configFile.Read(buffer)
-	//emit("\n %s\n", buffer)
-
-	buffer, err = StripComments(buffer) //去掉注释
-	if err != nil {
-		emit("Failed to strip comments from json: %s\n", err)
-		return &config
-	}
-
-	buffer = []byte(os.ExpandEnv(string(buffer))) //特殊
-
-	err = zgo.Utils.Unmarshal(buffer, &config) //解析json格式数据
-	if err != nil {
-		emit("Failed unmarshalling json: %s\n", err)
-		return &config
-	}
-	return &config
-}
-
-func StripComments(data []byte) ([]byte, error) {
-	data = bytes.Replace(data, []byte("\r"), []byte(""), 0) // Windows
-	lines := bytes.Split(data, []byte("\n"))                //split to muli lines
-	filtered := make([][]byte, 0)
-
-	for _, line := range lines {
-		match, err := regexp.Match(`^\s*#`, line)
-		if err != nil {
-			return nil, err
-		}
-		if !match {
-			filtered = append(filtered, line)
-		}
-	}
-
-	return bytes.Join(filtered, []byte("\n")), nil
-}
-
-func emit(msgfmt string, args ...interface{}) {
-	fmt.Printf(msgfmt, args...)
 }
