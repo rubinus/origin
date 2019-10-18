cb9e208cfc59b7ccc0094be09defd90d99f74b6e
朱大仙儿 <zhuhonglei@zhuge.com>
2019-10-18 13:18:21 +0800

    change default route

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env sh
+
+echo -e "\e[1;32m ************/etc/hosts************ \e[0m"
+
 cat /etc/hosts
+
+echo -e "\e[1;32m ************当前容器时间************ \e[0m"
+
+date
+
+echo -e "\e[1;32m ************容器Hostname********** \e[0m"
+
+hostname
+
+echo -e "\e[1;32m ************开始运行*************** \e[0m"
+
 ./origin --env dev
