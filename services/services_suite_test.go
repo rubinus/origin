f42a09ddc65300359403d9dd0020a087f96cb45f
朱大仙儿 <rubinus.chu@gmail.com>
2022-06-23 12:00:56 +0800

    feat: add test

--- services/services_suite_test.go
+++ services/services_suite_test.go
@@ -1,13 +1,50 @@
 package services_test
 
 import (
-	"testing"
-
+	"fmt"
+	"github.com/gitcpu-io/zgo"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"os"
+	"strings"
+	"testing"
+)
+
+const (
+	label_bj = "mongo_label_bj"
 )
 
 func TestServices(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Services Suite")
 }
+
+var _ = BeforeSuite(func() {
+	// 准备cpath
+	var cpath string
+	if cpath == "" {
+		pwd, err := os.Getwd()
+		if err == nil {
+			arr := strings.Split(pwd, "/")
+			cp := strings.Join(arr[:len(arr)-2], "/")
+			cpath = fmt.Sprintf("%s/%s", cp, "origin/configs")
+		}
+	}
+	//调用zgo
+	err := zgo.Engine(&zgo.Options{
+		CPath:   cpath,
+		Env:     "local",
+		Project: "origin",
+		Mongo: []string{
+			label_bj,
+			//label_sh,
+		},
+	})
+	//测试时表示使用mgo，在origin中使用一次
+	if err != nil {
+		panic(err)
+	}
+})
+
+var _ = AfterSuite(func() {
+})
--- services/weather_test.go
+++ services/weather_test.go
@@ -1,19 +1,63 @@
-package services_test
+package services
 
 import (
-	"fmt"
+	"context"
+	"github.com/gitcpu-io/origin/models/ioparams"
 	"github.com/gitcpu-io/origin/models/weather"
 	"github.com/gitcpu-io/origin/services/mocks"
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	"testing"
 )
 
 var _ = Describe("Weather", func() {
-	It("first test", func() {
-		ctrl := gomock.NewController(GinkgoT())
-		client := mocks.NewMockWeatherer(ctrl)
-		var result []*weather.Weather
-		client.EXPECT().List(gomock.Any(),gomock.Any()).Return(result,nil)
-		fmt.Println("success")
+	var s Weatherer
+	var city string
+	var svcobj *svc
+	var req *ioparams.WeatherRequest
+	Describe("List", func() {
+		BeforeEach(func() {
+			s = NewWeather()
+			gomega.Expect(s).NotTo(gomega.BeNil())
+			city = "深圳市"
+
+			svcobj = &svc{}
+			req = &ioparams.WeatherRequest{
+				Query:     "深圳市",
+			}
+		})
+		Context("begin", func() {
+			It("first test", func() {
+				ctrl := gomock.NewController(GinkgoT())
+				client := mocks.NewMockWeatherer(ctrl)
+				var result *weather.Weather
+				client.EXPECT().Insert(gomock.Any(),gomock.Any()).Return(result,nil)
+
+				_, err := s.List(context.TODO(),city)
+				gomega.Expect(err).To(gomega.BeNil())
+
+			})
+		})
+		Context("request", func() {
+			It("htt test", func() {
+
+				_, err := svcobj.dealRequestWeather(context.TODO(),req)
+				gomega.Expect(err).To(gomega.BeNil())
+
+			})
+		})
+
 	})
+
 })
+
+func BenchmarkDealRequestWeather(b *testing.B) {
+	s := &svc{}
+	req := &ioparams.WeatherRequest{
+		Query:     "深圳市",
+	}
+	for i := 0; i < b.N; i++ {
+		s.dealRequestWeather(context.TODO(),req)
+	}
+}
